43aa5d4b8d2d1cf19235b0d339a9da598733cc86
Dan Jarvis <danwakeem@gmail.com>
2017-08-18 10:13:23 -0500

    Added support for budrpt_e

--- tests/SQLGenerator.test.js
+++ tests/SQLGenerator.test.js
@@ -209,3 +209,48 @@ describe('SQL Generator individual function tests for Budrpt_a-90', () => {
 		expect(generator.createSelectStatement(true, 'Income Statement', 2016, 'Oct', 'acc', options)).to.be.equal(`SELECT a.GBCO,b.GMOBJ,b.GMSUB,b.GMLDA,trim(b.GMDL01) as GMDL01,SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2015 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 as PriorAnnualAct, SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2015 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as PriorYTDAct, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBBORG ELSE 0 END)/100 as CurrentOrigBud, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 as CurrentModBud, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 - SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 - SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentAvailBal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDBud, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentYTDEnc, SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDAct, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 +SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDTotal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDBud, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDEnc, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDAct, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 + SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDTotal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2017 THEN a.GBBORG ELSE 0 END)/100 as ProposedAnnualBud  FROM proddta.F0901 as b,proddta.F0902 as a  WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMCO in (companyKey) OR b.GMMCU in (BUKey))) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2015,2016,2017) OR a.GBFY IS NULL ))) GROUP BY a.GBCO,b.GMOBJ,b.BMSUB,b.GMLDA,trim(b.GMDL01) ORDER BY b.GMOBJ,b.GMSUB,b.GMLDA`)
 	})
 })
+
+describe('SQL Generator individual function tests for Budrpt_e-90', () => {
+	var Generator = require('./../lib/SQLGenerator')
+	var generator = new Generator({type: 'e'})
+
+	it('whereClause() should return valid businessLevel where partial', () => {
+		var data = {
+			buLevel: 'Comp',
+			key: 'Hello World',
+			accounts: 'acc',
+			forYear: 2017
+		}
+		expect(generator.whereClause(data)).to.be.equal(`WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMCO = 'Hello' )) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2016,2017,2018) OR a.GBFY IS NULL )))`)
+		data.buLevel = 'BusU'
+		expect(generator.whereClause(data)).to.be.equal(`WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMMCU = ' Hello World' )) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2016,2017,2018) OR a.GBFY IS NULL )))`)
+	})
+
+	it('whereClause() should return valid adHoc where partial', () => {
+		var data = {
+			companyKey: 'companyKey',
+			businessUnitKey: 'BUKey',
+			accounts: 'acc',
+			forYear: 2017
+
+		}
+		expect(generator.whereClause(data)).to.be.equal(`WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMCO in (companyKey) OR b.GMMCU in (BUKey))) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2016,2017,2018) OR a.GBFY IS NULL )))`)
+		
+		var oldCompanyKey = data.companyKey
+		data.companyKey = ''
+		expect(generator.whereClause(data)).to.be.equal(`WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMMCU in (BUKey))) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2016,2017,2018) OR a.GBFY IS NULL )))`)
+		
+		data.companyKey = oldCompanyKey
+		data.businessUnitKey = ''
+		expect(generator.whereClause(data)).to.be.equal(`WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMCO in (companyKey))) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2016,2017,2018) OR a.GBFY IS NULL )))`)
+	})
+	
+	it('createSelectStatement() should genearate a valid select statement', () => {
+		var options = {
+			companyKey: 'companyKey',
+			businessUnitKey: 'BUKey'
+		}
+		
+		expect(generator.createSelectStatement(true, 'Income Statement', 2016, 'Oct', 'acc', options)).to.be.equal(`SELECT a.GBCO,b.GMOBJ,b.GMSUB,b.GMLDA,trim(b.GMDL01) as GMDL01,SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2015 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 as PriorAnnualAct, SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2015 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as PriorYTDAct, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBBORG ELSE 0 END)/100 as CurrentOrigBud, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 as CurrentModBud, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAN01+a.GBAN02+a.GBAN03+a.GBAN04+a.GBAN05+a.GBAN06+a.GBAN07+a.GBAN08+a.GBAN09+a.GBAN10+a.GBAN11+a.GBAN12+a.GBAN13 ELSE 0 END)/100 - SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 - SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentAvailBal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDBud, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentYTDEnc, SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDAct, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 +SUM(CASE WHEN a.GBLT = 'AA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01-a.GBAPYC ELSE 0 END)/100 as CurrentYTDTotal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDBud, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDEnc, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDAct, SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 + SUM(CASE WHEN a.GBLT = 'PA' and a.GBFY = 2016 THEN a.GBAPYC+a.GBAN01 ELSE 0 END)/100 as CurrentPTDTotal, SUM(CASE WHEN a.GBLT = 'BA' and a.GBFY = 2017 THEN a.GBBORG ELSE 0 END)/100 as ProposedAnnualBud  FROM proddta.F0901 as b,proddta.F0902 as a  WHERE ( (b.GMAID = a.GBAID(+)) AND ((b.GMOBJ acc)) AND ((b.GMCO in (companyKey) OR b.GMMCU in (BUKey))) AND ((a.GBLT in ('AA','PA','BA','B3') OR a.GBLT IS NULL )) AND ((a.GBFY in (2015,2016,2017) OR a.GBFY IS NULL ))) GROUP BY a.GBCO,b.GMOBJ,b.BMSUB,b.GMLDA,trim(b.GMDL01) ORDER BY b.GMOBJ,b.GMSUB,b.GMLDA`)
+	})
+})
