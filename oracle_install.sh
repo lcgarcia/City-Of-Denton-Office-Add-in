b6627145bbc176b861d11fe23f131a7d3da98816
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 14:07:58 -0500

    removed symbolic link

--- oracle_install.sh
+++ oracle_install.sh
@@ -4,7 +4,8 @@ unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
 unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip
 mv instantclient_12_2 instantclient
 cd instantclient
-ln -s libclntsh.so.12.1 libclntsh.so
+cp libclntsh.so.12.1 libclntsh.so
+# ln -s libclntsh.so.12.1 libclntsh.so
 ls -la 
 ls -la instantclient
 # Set oracle sdk in path
