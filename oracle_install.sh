8100af0a26fcd33d3c0de4db03e7482e07321d86
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 14:47:04 -0500

    Sourced oracle_install

--- oracle_install.sh
+++ oracle_install.sh
@@ -8,11 +8,11 @@ cp libclntsh.so.12.1 libclntsh.so
 # ln -s libclntsh.so.12.1 libclntsh.so
 
 # Set oracle sdk in path
-# export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-# export OCI_LIB_DIR=$PWD
-# export OCI_INC_DIR=$PWD/sdk/include
-# echo $OCI_LIB_DIR
-# echo $OCI_INC_DIR
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+export OCI_LIB_DIR=$PWD
+export OCI_INC_DIR=$PWD/sdk/include
+echo $OCI_LIB_DIR
+echo $OCI_INC_DIR
 ls -la 
 cd ../..
 source pathvars.sh
