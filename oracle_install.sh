ab9e0ba9b6057b8a0f5a15c59deabd91a56076c1
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 13:08:38 -0500

    Try preinstall script

--- oracle_install.sh
+++ oracle_install.sh
@@ -2,7 +2,7 @@
 unzip linux_zips/instantclient-basic-linux.x64-12.2.0.1.0.zip -d linux_zips
 unzip linux_zips/instantclient-sdk-linux.x64-12.2.0.1.0.zip -d linux_zips
 mv linux_zips/instantclient_12_2 linux_zips/instantclient
-ln -s linux_zips/instantclient/libclntsh.so.12.1 linux_zips/instantclient/libclntsh.so
+ln -s $PWD/linux_zips/instantclient/libclntsh.so.12.1 $PWD/linux_zips/instantclient/libclntsh.so
 # Set oracle sdk in path
 export LD_LIBRARY_PATH=$PWD/linux_zips/instantclient:$LD_LIBRARY_PATH
 export OCI_LIB_DIR=$PWD/linux_zips/instantclient
