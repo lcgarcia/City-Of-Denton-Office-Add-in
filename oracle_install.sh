97a86a1f2cd07775cc0f0539b771df9f481ef104
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 11:48:03 -0500

    Removed large files

--- oracle_install.sh
+++ oracle_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-unzip linux_zips/instantclient-basic-linux.x64-12.2.0.1.0.zip
-unzip linux_zips/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+# unzip linux_zips/instantclient-basic-linux.x64-12.2.0.1.0.zip
+# unzip linux_zips/instantclient-sdk-linux.x64-12.2.0.1.0.zip
+wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/libociei.so" -P linux_zips/instantclient_12_2
 mv linux_zips/instantclient_12_2 instantclient
 cd linux_zips/instantclient
 ln -s linux_zips/instantclient/libclntsh.so.12.1 linux_zips/instantclient/libclntsh.so
