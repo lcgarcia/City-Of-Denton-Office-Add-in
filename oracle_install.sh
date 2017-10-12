a0b34b9d9bbe567a4d2949031679edfce8c1a485
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 11:55:02 -0500

    Fixed oracle_install

--- oracle_install.sh
+++ oracle_install.sh
@@ -2,8 +2,7 @@
 # unzip linux_zips/instantclient-basic-linux.x64-12.2.0.1.0.zip
 # unzip linux_zips/instantclient-sdk-linux.x64-12.2.0.1.0.zip
 wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/libociei.so" -P linux_zips/instantclient_12_2
-mv linux_zips/instantclient_12_2 instantclient
-cd linux_zips/instantclient
+mv linux_zips/instantclient_12_2 linux_zips/instantclient
 ln -s linux_zips/instantclient/libclntsh.so.12.1 linux_zips/instantclient/libclntsh.so
 # Set oracle sdk in path
 export LD_LIBRARY_PATH=$PWD/linux_zips/instantclient:$LD_LIBRARY_PATH
