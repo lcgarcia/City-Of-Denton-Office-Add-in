3be88e7835aabe62ce4e75e9d339c3dde1c369f9
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 14:06:10 -0500

    Public path

--- oracle_install.sh
+++ oracle_install.sh
@@ -6,11 +6,11 @@ mv instantclient_12_2 instantclient
 cd instantclient
 ln -s libclntsh.so.12.1 libclntsh.so
 ls -la 
-ls -la linux_zips/instantclient
+ls -la instantclient
 # Set oracle sdk in path
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-export OCI_LIB_DIR=$PWD:$OCI_LIB_DIR
-export OCI_INC_DIR=$PWD/sdk/include:$OCI_INC_DIR
+export LD_LIBRARY_PATH=$PWD/:$LD_LIBRARY_PATH
+export OCI_LIB_DIR=$PWD/:$OCI_LIB_DIR
+export OCI_INC_DIR=$PWD/sdk/include/:$OCI_INC_DIR
 echo $OCI_LIB_DIR
 echo $OCI_INC_DIR
 cd ../..
