9bad47a2f8145842b00cdb36ed04ff5c17bf0abf
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 14:01:19 -0500

    Public path

--- oracle_install.sh
+++ oracle_install.sh
@@ -9,8 +9,8 @@ ls -la
 ls -la linux_zips/instantclient
 # Set oracle sdk in path
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-export OCI_LIB_DIR=$PWD
-export OCI_INC_DIR=$PWD/sdk/include
+export OCI_LIB_DIR=$PWD:$OCI_LIB_DIR
+export OCI_INC_DIR=$PWD/sdk/include:$OCI_INC_DIR
 echo $OCI_LIB_DIR
 echo $OCI_INC_DIR
 cd ../..
