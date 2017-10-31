d4907e0b0eeec15f0e58c49ee56697be78b07133
Dan Jarvis <danwakeem@gmail.com>
2017-10-31 08:42:28 -0500

    Inital Azure setup

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 if (!('VCAP_SERVICES' in process.env)) {
   require('dotenv').config({ path: process.env.ENVPATH });
 }
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const passport = require('passport'),
+    OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const config = require('./config');
+const CloudantStore = require('connect-cloudant-store')(session);
+const store = new CloudantStore({
+  url: JSON.parse(process.env.VCAP_SERVICES).cloudantNoSQLDB[0].credentials.url,
+  database: 'sessions'
+});
 
-var routes = require('./routes/index');
-var budget = require('./routes/budget');
-var book = require('./routes/book');
-var datasource = require('./routes/datasource');
-var job = require('./routes/job');
+const routes = require('./routes/index');
+const budget = require('./routes/budget');
+const book = require('./routes/book');
+const datasource = require('./routes/datasource');
+const job = require('./routes/job');
 
 var app = express();
 
@@ -31,7 +40,98 @@ app.use('/node_modules/angular', express.static(__dirname + '/node_modules/angul
 app.use('/node_modules/office-ui-fabric-js', express.static(__dirname + '/node_modules/office-ui-fabric-js'));
 app.use('/node_modules/client-js', express.static(__dirname + '/node_modules/client-js'));
 app.use('/node_modules/lodash', express.static(__dirname + '/node_modules/lodash')); 
-app.use('/node_modules/core-js', express.static(__dirname + '/node_modules/core-js')); 
+app.use('/node_modules/core-js', express.static(__dirname + '/node_modules/core-js'));
+
+passport.serializeUser(function(user,done){
+  done(null,user);
+});
+passport.deserializeUser(function(user,done){
+  done(null,user);
+});
+
+app.use(session({
+  secret: 'lasjflju**(()U0990U87g90o211',
+  store: store,
+  saveUninitialized: false,
+  resave: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new OIDCStrategy({
+  identityMetadata: config.creds.identityMetadata,
+  clientID: config.creds.clientID,
+  responseType: config.creds.responseType,
+  responseMode: config.creds.responseMode,
+  redirectUrl: config.creds.redirectUrl,
+  allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+  clientSecret: config.creds.clientSecret,
+  validateIssuer: config.creds.validateIssuer,
+  isB2C: config.creds.isB2C,
+  issuer: config.creds.issuer,
+  passReqToCallback: config.creds.passReqToCallback,
+  scope: config.creds.scope,
+  loggingLevel: config.creds.loggingLevel,
+},
+function(req, iss, sub, profile, accessToken, refreshToken, done) {
+  if (!profile.oid) {
+    return done(new Error("No oid found"), null);
+  } else {
+    return done(null, profile);
+  }
+}
+));
+
+const userCheck = (req, res, next) => {
+  if(req.user) {
+    req.session.nowInMinutes = Date.now() / 60e3;
+    next();
+  } else
+    res.redirect('/');
+}
+
+app.get('/login',
+function(req, res, next) {
+  passport.authenticate('azuread-openidconnect', 
+    { 
+      response: res,
+      resourceURL: config.resourceURL,
+      customState: 'my_state',
+      failureRedirect: '/?error=login' 
+    }, (err, user, info) => {
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.redirect('/');
+      });
+    }
+  )(req, res, next);
+},
+function(req, res) {
+  console.log('Login was called in the Sample')
+  res.redirect('/');
+});
+
+app.post('/auth/openid/return',
+function(req, res, next) {
+  passport.authenticate('azuread-openidconnect', 
+    { 
+      response: res,
+      failureRedirect: '/?error=return'  
+    }
+  )(req, res, next);
+},
+function(req, res) {
+  console.info('We received a return from AzureAD.');
+  res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.session.destroy(function(err) {
+    req.logOut();
+    res.redirect(config.destroySessionUrl);
+  });
+});
 
 app.use('/', routes);
 app.use('/ks2inc/budget', budget);
